29ede0b12e75e37f6752ce24a3f80f3111db3157
Hackbright Student <info@hackbrightacademy.com>
2014-10-08 12:58:51 -0700

    working dictionary, markov chain in process

--- markov.py
+++ markov.py
@@ -34,20 +34,44 @@ def make_text(chains):
     for k in chains.keys():
         key_list.append(k)
     # print key_list
-
     starting_point = random.choice(key_list)
-    count = 0 
-    while count < 10:
-        key = starting_point
-        # print "key", key
-        chain_one = random.choice(chains[key])
+    # count = 0
+    markov_output_string = ""
+    markov_output_string += starting_point[0] + " " + starting_point[1] + " "
+    key = starting_point
+    # print "key=", key
+    chain_one = random.choice(chains[key])
+    # print "chain_one =", chain_one
+    next_tuple = (key[1], chain_one)
+    markov_output_string += chain_one + " "
+    # print "this is the markov string =", markov_output_string
+    print "what the next tuple will look like =", next_tuple
+    
+    while True:
+        if next_tuple in chains: 
+            # next_tuple = (key[1], chain_one)
+            next_value = random.choice(chains[next_tuple])
+            markov_output_string += next_value + " "
+            print markov_output_string
+            next_tuple = (next_tuple[1], next_value)
+        else:
+            print "tuple wasn't found", markov_output_string
+            return# or break 
         # print "value", chain_one
         #find this tuple
-        if chains[(key[1], chain_one)] in chains:
-            #new_chain returns a random value from this tuple
-            new_chain = random.choice(chains[(key[1], chain_one)])
-            print new_chain
-        count += 1
+        #print key
+        # print key[1]
+        #print chain_one
+        # if chains[(key[1], chain_one)] in chains:
+        #     #new_chain returns a random value from this tuple
+        #     new_chain = random.choice(chains[(key[1], chain_one)])
+        #     print new_chain
+        # count += 1
+
+
+
+#markov_output_string += ________
+
 
 
 def main():
